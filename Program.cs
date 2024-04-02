5f5488c7e93413d2cfb728a0e8338ed0e1dd2883
User@ETYCZNY <biurohetmana@gmail.com>
2024-04-02 16:44:50 +0100

    Licznik średniej

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    public static void main(String[] args) 
     {
-        Console.WriteLine("Hello, World!");
+       
+      
+    int i = 0;
+    int z = 0;
+    double ce;
+    int j = -1;
+    double x;
+    Boolean exit = false;
+        while (!exit){
+            ce = (double.Parse(Console.ReadLine()));
+            i++;
+            if (ce == j){
+                i--;
+                x = (double)z / i;
+    Console.WriteLine(x);
+                break;
+            }else z = (int)(z + ce);
+            
+        }
+
     }
 }
